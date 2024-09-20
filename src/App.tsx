e53dd8efe12b4a2a04f1a39f160ca65cb9596c52
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-09-20 14:46:45 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HelloWorld from "./component/HelloWorld.tsx";
+
+export default function App() {
+
+
+    return (
+        <>
+            <HelloWorld/>
+        </>
+    )
+}
+
